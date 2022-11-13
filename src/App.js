ddc3e48b9bb73b94494300c24703d93c5cf8afa5
ynainggolan <yhpnainggolan21@gmail.com>
2022-11-12 19:55:13 -0500

    added packages and shuffle function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-//import logo from './logo.svg';
 import './App.css';
 import Card from './Card'
+import { useState } from 'react'
+
+const cardValues = [
+    "Hello", "Goodbye", "Maybe",
+     "Hey", "What", "Why", "How"
+]
 
 function App(){
-    const cardValues = ["Hello", "Goodbye", "Maybe", "Hey", 
-    "Hello", "Goodbye", "Maybe", "Hey", "What", "Why", "How", 
-    "What", "Why", "How"]
-    const newGame= <button onclick="newGame()">New Game</button>;
-    <button>New Game</button>
-    return(
-        <div className="App">
-            <button>New Game</button>
+    const [cards, setCards] = useState([])
+    const [turns, setTurns] = useState([])
+    //shuffle cards
+    const shuffleCards =() =>{
+        //const shuffleCards = [...cardValues, ...cardValues].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id:Math.random()}))
+        const shuffleCards = [...cardValues, ...cardValues].sort(() => Math.random() - 0.5)
 
-            <div className="card-grid">
+        setCards(shuffleCards)
+        setTurns(0)
+    }
+   
+    console.log(cards, turns)
+/*<div className="card-grid">
             {cardValues.map((item,idx)=>(<Card key={idx} cardText={item}/>))}
             </div>
+            {cards.map(card => (<Card key={card.id} card={card}/>))}*/ 
+    return(
+        <div className="App">
+            <button onClick={shuffleCards}>New Game</button>
+        <div className="card-grid">
+            {cards.map((item,idx)=>(<Card key={idx} cardText={item}/>))}
+        </div>
             
         </div>
     );
